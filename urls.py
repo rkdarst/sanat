f8ba68b95943d46f2f5f9d27adacb464c5a54a24
Richard Darst <rkd@zgib.net>
2013-03-25 19:31:54 +0200

    Initial commit
    Ignore-this: c0d1f17b7661936741e794d7bde28103
    
    darcs-hash:20130325173154-82ea9-7c966aeca718a1d9352f17d3a5d79071851975bc

---
+++ urls.py
@@ -0,0 +1,19 @@
+from django.conf.urls.defaults import *
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Example:
+    # (r'^learn/', include('learn.foo.urls')),
+    #(r'/', include('learn.vocab.urls')),
+    (r'^run/', 'vocab.views.run'),
+    (r'^',     'vocab.views.select'),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    (r'^admin/', include(admin.site.urls)),
+)
