c77766292d6c50d850c508aca546517744e8d4c3
Richard Darst <rkd@zgib.net>
2015-05-10 10:57:11 +0300

    Initial database schema (old)

--- models.py
+++ models.py
@@ -8,14 +8,31 @@ from config import app
 db = SQLAlchemy(app)
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    uid = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True)
     email = db.Column(db.String(120), unique=True)
-
     def __init__(self, username, email):
         self.username = username
         self.email = email
-
     def __repr__(self):
         return '<User %r>' % self.username
+class UserAttributes(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    uid = db.relationship('User', backref='answers', lazy='dynamic')
 
+
+class Answer(db.Model):
+    aid = db.Column(db.Integer, primary_key=True) # answer ID
+    # user ID
+    user = db.relationship('User', backref='answers', lazy='dynamic')
+    sid = db.Column(db.Integer)                   # session ID
+    ts = db.Column(db.DateTime)
+    q = db.Column(db.String(256))
+    a = db.Column(db.String(256))
+    correct = db.Column(db.Boolean)
+
+
+class Hint(db.Model):
+    hid = db.Column(db.Integer, primary_key=True)
+    ts = db.Column(db.DateTime)
+    text = db.Column(db.String(256))
